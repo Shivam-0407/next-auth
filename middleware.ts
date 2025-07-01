2509702aed783bf99c1d912a6f817c9151411314
shivam <shivamt0407@gmail.com>
2025-07-01 17:57:38 +0530

    feat(middleware): add the middleware for redirecting the verified user

---
+++ middleware.ts
@@ -0,0 +1,48 @@
+import { auth } from "@/auth";
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiPrefix,
+  authRoutes,
+  publicRoutes,
+} from "./routes";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+
+  const isLoggedIn = !!req.auth;
+
+  const isAPIRoute = nextUrl.pathname.startsWith(apiPrefix);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  if (isAPIRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      // whenever we're creating new URL , due the 2nd argument we can create the full link
+      // i.e http://localhost:3000/DEFAULT_LOGIN_REDIRECT
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    console.log("calling should be here!!");
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return null;
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
