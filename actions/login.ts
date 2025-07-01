dc1db02f17855aef2b8dafd261d61844451bccba
shivam <shivamt0407@gmail.com>
2025-07-01 17:59:07 +0530

    feat(login): update the login to for validated users

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,10 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
+import { match } from "ts-pattern";
 import { z } from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -9,5 +13,23 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return match(error.type)
+        .with("CredentialsSignin", () => ({
+          error: "Invalid Credentials!!",
+        }))
+        .otherwise(() => ({
+          error: "Something went wrong!!",
+        }));
+    }
+  }
 };
