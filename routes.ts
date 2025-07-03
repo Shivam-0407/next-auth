4d24c1ba409ef5c128156443c41829925561b430
shivam <shivamt0407@gmail.com>
2025-07-04 01:16:10 +0530

    feat(route): add new public routes & auth-routes

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * @type {string[]}
  */
 
-export const publicRoutes = ["/"];
+export const publicRoutes = ["/","/auth/new-verification"];
 
 /**
  * An array of routes accessible to the public which will require authentication
@@ -11,7 +11,7 @@ export const publicRoutes = ["/"];
  * @type {string[]}
  */
 
-export const authRoutes = ["/auth/login", "/auth/register"];
+export const authRoutes = ["/auth/login", "/auth/register","/auth/error","/auth/reset"];
 
 /**
  * The API for authentication routes
