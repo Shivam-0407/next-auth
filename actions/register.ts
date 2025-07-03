b5514937c24a242fd3f5a11dd019e301abfe074e
shivam <shivamt0407@gmail.com>
2025-07-04 01:23:03 +0530

    feat(new-verification;register;rest): add new actions  of new-verification,register & reset

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,8 @@ import { z } from "zod";
 import bcryptjs from "bcryptjs";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/users";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -32,5 +34,8 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   // Send verification token email
 
-  return { success: "User  created" };
+  const verificationToken = await generateVerificationToken(email);
+
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+  return { success: "Confirmation email sent" };
 };
