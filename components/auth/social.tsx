e7032a16f98f06f1b2a416f78d30714e6f0c8f30
shivam <shivamt0407@gmail.com>
2025-07-04 01:21:10 +0530

    refactor(social): add the sign for OAuth providers for github & google in social component

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,14 +2,32 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export default function Social() {
+  const onClick = (providers: "github" | "google") => {
+    signIn(providers, {
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" variant="outline" onClick={() => {}} className="flex-1">
+      <Button
+        size="lg"
+        variant="outline"
+        onClick={() => onClick("google")}
+        className="flex-1"
+      >
         <FcGoogle className="w-5 h-5" />
       </Button>
-      <Button size="lg" variant="outline" onClick={() => {}} className="flex-1">
+      <Button
+        size="lg"
+        variant="outline"
+        onClick={() => onClick("github")}
+        className="flex-1"
+      >
         <FaGithub className="w-5 h-5" />
       </Button>
     </div>
