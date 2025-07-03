1b9f8da5c293562cb5a68b89bac1b94b22734c60
shivam <shivamt0407@gmail.com>
2025-07-04 01:20:08 +0530

    feat(page,reset-form): add new page for reset; create reset-form

---
+++ components/auth/reset-form.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import CardWrapper from "./card-wrapper";
+import { ResetSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import FormError from "@/components/form-error";
+import FormSuccess from "@/components/form-success";
+import { useState, useTransition } from "react";
+import { reset } from "@/actions/reset";
+
+export default function ResetForm() {
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [error, setError] = useState<string | undefined>("");
+
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
+    setError("");
+    setSuccess("");
+    startTransition(() => {
+      reset(values).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Forgot Password"
+      backButtonLabel="Back To Login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="johndoe@gmail.com"
+                      type="email"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button className="w-full" type="submit" disabled={isPending}>
+            Send reset email
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
