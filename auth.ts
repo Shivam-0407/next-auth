cd1244ed6a7586d8de6fb5922a29d4311ef4268e
shivam <shivamt0407@gmail.com>
2025-07-04 01:15:40 +0530

    feat(auth): add Google Provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth, { type DefaultSession } from "next-auth";
 import bcryptjs from "bcryptjs";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
@@ -9,7 +10,7 @@ import { getUserByEmail, getUserByID } from "./data/users";
 import { UserRole } from "@prisma/client";
 
 export type ExtendedUser = DefaultSession["user"] & {
-  role: "ADMIN" | "USER";
+  role: UserRole;
 };
 
 declare module "next-auth" {
@@ -20,6 +21,10 @@ declare module "next-auth" {
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
     GithubProvider({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
@@ -57,7 +62,38 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: "jwt",
   },
+  events: {
+    // used for kicking sideEffects like here if a person is sigining in via Oauth,
+    // then we would like him to be verified as well
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
   callbacks: {
+    async signIn({ user, account }) {
+
+      //Allow Oauth without verification email
+      if (account?.provider !== "credentials") {
+        return true;
+      }
+
+      const existingUser = await getUserByID(user.id);
+
+      // Prevent sign-in without verfication
+      if (!existingUser?.emailVerified) {
+        return false;
+      }
+
+      //Todo: Add 2FA
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
@@ -83,6 +119,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   secret: process.env.AUTH_SECRET,
   pages: {
-    signIn: "/auth/login",
+    signIn: "/auth/login", // this will be rendererd when something goes wrong even whent
+    // the user is trying to access the same account resposible for github & google
+    error: "/auth/error", // this will be rendered when something breaks
   },
 });
