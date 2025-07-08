e25463f05483a11a677e280b4b10151c205ec9a9
shivam <shivamt0407@gmail.com>
2025-07-09 05:07:54 +0530

    feat(layout): add new layout component

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,12 @@
+type ProtectedLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  return (
+    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[linear-gradient(90deg,_rgba(131,58,180,1)_0%,_rgba(253,29,29,1)_50%,_rgba(252,176,69,1)_100%)]">
+      {children}
+    </div>
+  );
+}
+
