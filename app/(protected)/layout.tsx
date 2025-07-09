35b8489e81740580d7f37c9c4c9e9e8850ad3913
shivam <shivamt0407@gmail.com>
2025-07-09 06:42:42 +0530

    refactor(layout): add Toaster to the root layout; add Navbar to the protected layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "./_components/navbar";
+
 type ProtectedLayoutProps = {
   children: React.ReactNode;
 };
@@ -5,8 +7,8 @@ type ProtectedLayoutProps = {
 export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
   return (
     <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[linear-gradient(90deg,_rgba(131,58,180,1)_0%,_rgba(253,29,29,1)_50%,_rgba(252,176,69,1)_100%)]">
+      <Navbar />
       {children}
     </div>
   );
 }
-
