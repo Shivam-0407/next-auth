84168d08f3c75288bcbfbde4629bb321a60abb65
shivam <shivamt0407@gmail.com>
2025-07-10 16:00:05 +0530

    feat(page): remove the commented code; add asChild property for the LoginButton

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
           🔒 Auth
         </div>
         <p className="text-white text-lg">A simple authentication service</p>
-        <LoginButton>
-          <Button variant="secondary" size={"lg"} >
+        <LoginButton asChild>
+          <Button variant="secondary" size={"lg"}>
             Sign In
           </Button>
         </LoginButton>
